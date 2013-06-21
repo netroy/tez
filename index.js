dd48a2eaa180c3fadd3825bca15cb12ca095082e
Aditya <aditya@netroy.in>
2013-06-21 15:19:12 +0200

    websocket support

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var PGClient = require('pg').Client;
+var WebSocketServer = require('ws').Server;
+var EventEmitter = require('events').EventEmitter;
+var http = require('http');
 
 var connUrl = 'tcp://localhost/wunderapi';
 var tables = {
@@ -9,6 +12,7 @@ var tables = {
 };
 var countQuery = 'SELECT relname as table, n_live_tup as count FROM pg_stat_user_tables';
 
+var emitter = new EventEmitter();
 var client = new PGClient(connUrl);
 var connection = client.connection;
 
@@ -31,8 +35,10 @@ function sync () {
 
 connection.on('notification', function (message) {
   message = message || {};
-  if (message.channel === 'creation' && message.payload in tables) {
-    tables[message.payload]++;
+  var table_name = message.payload;
+  if (message.channel === 'creation' && table_name in tables) {
+    var count = ++tables[table_name];
+    emitter.emit('creation', table_name, count);
   }
 });
 
@@ -43,3 +49,38 @@ connection.once('readyForQuery', function () {
 });
 
 client.connect();
+
+var server = http.createServer(function (req, resp) {
+  if (req.url === '/') {
+    resp.writeHead(200, {
+      'Content-Type': 'application/json'
+    });
+    resp.end(JSON.stringify(tables));
+  } else {
+    resp.writeHead(302, {
+      'Location': '/'
+    });
+    resp.end();
+  }
+});
+
+new WebSocketServer({
+  'server': server
+}).on('connection', function(client) {
+
+  // signup this connection for changes
+  function notify (table_name, count) {
+    var obj = {};
+    obj[table_name] = count;
+    client.send(JSON.stringify(obj));
+  }
+  client.on('close', function () {
+    emitter.removeListener('creation', notify);
+  });
+  emitter.addListener('creation', notify);
+
+  // send all the stats on first connect
+  client.send(JSON.stringify(tables));
+});
+
+server.listen(process.env.PORT || 1337);
